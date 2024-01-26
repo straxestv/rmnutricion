e0a216b82b193c6aa7264ca98159ef9ee0560f4d
straxestv <eduardgamerx66@gmail.com>
2024-01-26 02:33:16 -0600

    sss

--- backend/server.js
+++ backend/server.js
@@ -59,5 +59,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`serve at srv467208.hstgr.cloud`);
 });
